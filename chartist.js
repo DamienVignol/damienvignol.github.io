11f413265d78c53dfb7f92873f3363eb68997730
Damien Vignol <damienvignol@gmail.com>
2016-09-30 09:07:40 +0200

    Add files via upload

---
+++ chartist.js
@@ -0,0 +1,24 @@
+/*
+var chartData = {
+  series: [parseInt(Cent), parseInt(Lib), parseInt(Mod), parseInt(Kri), parseInt(Soc), parseInt(Van), parseInt(Mil), parseInt(Sd)]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('.ct-chart', chartData, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / chartData.series.reduce(sum) * 100) + '%';
+  }
+});
+
+
+new Chartist.Pie2('.ct-chart2', {
+  series: chartData
+}, {
+  donut: true,
+  donutWidth: 60,
+  startAngle: 270,
+  total: 200,
+  showLabel: false
+});
+*/
